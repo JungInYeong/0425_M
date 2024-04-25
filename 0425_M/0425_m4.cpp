9b41b419fd9c734871db0db4d74837f079be2f50
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-04-25 20:23:59 +0900

    ㅇㅇ

---
+++ 0425_M/0425_m4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int student;
+	double sum = -0;
+	cout << "몇 명의 학생입니까? : ";
+	cin >> student;
+
+
+	int* grad = new int[student];
+	for (int i = 0; i < student; i++)
+	{
+		cout << i + 1 << "번 학생의 성적을 입력하세요 : ";
+		cin >> grad[i];
+		sum = grad[i] + sum;
+
+		if (student == i + 1)
+		{
+			cout << "성적 평균 : " << sum/student;
+		}
+	}
+
+
+	return 0;
+}
