a316ef4344adb709c9a6075c47507ec1ed656420
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-04-25 20:30:22 +0900

    ㅇㅇ

--- 0425_M/0425_m4.cpp
+++ 0425_M/0425_m4.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main()
 {
 	int student;
-	double sum = 0;
+	double sum = 0; //평균을 위한 성적 덧셈
+
 	cout << "몇 명의 학생입니까? : ";
 	cin >> student;
 
-
+	// 동적
 	int* grad = new int[student];
+
+	// 시작
 	for (int i = 0; i < student; i++)
 	{
 		cout << i + 1 << "번 학생의 성적을 입력하세요 : ";
